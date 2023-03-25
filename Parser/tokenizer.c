66d2b76a8cd4976139edb1080eed49d9f875d3b2
Batuhan Taskaya <batuhan@python.org>
2023-03-25 05:30:59 +0300

    Top-level '}' should be rejected directly

--- Parser/tokenizer.c
+++ Parser/tokenizer.c
@@ -2378,6 +2378,9 @@ tok_get_normal_mode(struct tok_state *tok, tokenizer_mode* current_tok, struct t
     case ']':
     case '}':
         if (!tok->level) {
+            if (tok->tok_mode_stack_index > 0 && !current_tok->bracket_stack && c == '}') {
+                return MAKE_TOKEN(syntaxerror(tok, "f-string: single '}' is not allowed"));
+            }
             return MAKE_TOKEN(syntaxerror(tok, "unmatched '%c'", c));
         }
         tok->level--;
